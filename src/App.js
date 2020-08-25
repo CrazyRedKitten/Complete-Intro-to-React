d0f5a60c977ab2ec3b5417b4d34b732ae798f598
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-08-25 13:07:43 +0300

    Hooks | Creating a Search Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something important">Adopt Me!</h1>
-      <Pet name="Alien" animal="cat" breed="Maine Coon" />
-      <Pet name="Signal" animal="cat" breed="Sphynx" />
-      <Pet name="Flexik" animal="cat" breed="Persian" />
+      <SearchParams />
     </div>
   );
 };
