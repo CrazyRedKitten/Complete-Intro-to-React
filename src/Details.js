cc4ee4e66864497d23563b633c7471d5e44241fe
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-08-27 18:53:48 +0300

    Class Components | Class Components

--- src/Details.js
+++ src/Details.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Router, Link } from "@reach/router";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-  //DEBUG
-  // <pre>
-  //   <code>{JSON.stringify(props, null, 4)}</code>
-  // </pre>
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState(
+        {
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        },
+        console.error
+      );
+    });
+  }
+  render() {
+    return <div></div>;
+  }
+}
 
 export default Details;
