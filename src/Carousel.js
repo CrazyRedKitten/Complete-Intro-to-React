bd3c12e8690fdbc8134ae81f24d98fcc2a71f1b0
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-08-28 12:03:00 +0300

    Class Components | Carousel Implementation

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Carousel extends Component {
+class Carousel extends React.Component {
   state = {
     photos: [],
     active: 0,
   };
-
   static getDerivedStateFromProps({ media }) {
     let photos = ["http://placecorgi.com/600/600"];
 
@@ -15,25 +14,26 @@ export class Carousel extends Component {
 
     return { photos };
   }
-
-  handleEventClick = (event) => {
-    this.setState({ active: +event.target.dataset.index });
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
   };
-
   render() {
     const { photos, active } = this.state;
-
     return (
       <div className="carousel">
         <img src={photos[active]} alt="animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
-              src={photo}
-              alt="Animal thumbnail"
+              onClick={this.handleIndexClick}
               data-index={index}
+              src={photo}
               className={index === active ? "active" : ""}
+              alt="animal thumbnail"
             />
           ))}
         </div>
--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,14 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
   state = { loading: true };
   componentDidMount() {
-    pet.animal(this.props.id).then(({ animal }) => {
-      this.setState(
-        {
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
           name: animal.name,
           animal: animal.type,
           location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -14,24 +16,23 @@ class Details extends React.Component {
           media: animal.photos,
           breed: animal.breeds.primary,
           loading: false,
-        },
-        // eslint-disable-next-line no-console
-        console.error
-      );
-    });
+        });
+      })
+      .catch((err) => this.setState({ error: err }));
   }
   render() {
-    if (this.setState.loading) {
-      return <h1>Loading</h1>;
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
     }
 
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, media, name } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
-          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
           <button>Adopt {name}</button>
           <p>{description}</p>
         </div>
